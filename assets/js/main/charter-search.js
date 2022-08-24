d46823a468d61e903ced5ec6a18f216088d0e61a
bevenken <benjamin.venken@ugent.be>
2022-08-24 10:06:24 +0000

    Component now gets loaded in CharterSearchApp.vue, where it gets used

--- assets/js/main/charter-search.js
+++ assets/js/main/charter-search.js
@@ -1,10 +1,6 @@
 import Vue from 'vue'
 import CharterSearchApp from '../apps/CharterSearchApp'
 
-import fieldDMYRange from '../components/FormFields/fieldDMYRange'
-
-Vue.component('fieldDMYRange', fieldDMYRange)
-
 new Vue({
     el: '#charter-search-app',
     components: {
