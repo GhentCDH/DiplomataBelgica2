3fdcdf5eb7689183bcd0c1f1034e14d07b129990
Frederic Lamsens <frederic.lamsens@ugent.be>
2022-09-23 11:36:38 +0200

    added grid prop

--- assets/js/components/Sidebar/LabelValue.vue
+++ assets/js/components/Sidebar/LabelValue.vue
@@ -43,6 +43,10 @@ export default {
             type: String,
             default: null
         },
+        grid: {
+            type: String,
+            default: '6|6'
+        },
         type: {
             type: String,
             default: 'string'
@@ -57,11 +61,17 @@ export default {
         }
     },
     computed: {
+        labelWidth() {
+            return this.grid.split('|')[0] ?? 6;
+        },
+        valueWidth() {
+            return this.grid.split('|')[1] ?? 6;
+        },
         outputLabelClass() {
-            return ['labelvalue__label', this.inline ? 'labelvalue__label--inline col-6 ellipsis' : 'col-12 ellipsis', this.labelClass ? this.labelClass : ''].join(' ')
+            return ['labelvalue__label', this.inline ? 'labelvalue__label--inline col-' + this.labelWidth : 'col-12', this.labelClass ?? ''].join(' ')
         },
         outputValueClass() {
-            return ['labelvalue__value', this.inline ? 'labelvalue__value--inline col-6 ellipsis' : 'col-12 ellipsis', this.valueClass ? this.valueClass : ''].join(' ')
+            return ['labelvalue__value', this.inline ? 'labelvalue__value--inline col-' + this.valueWidth : 'col-12', this.valueClass ?? ''].join(' ')
         },
         outputValues() {
             let values = this.value ? ( Array.isArray(this.value) ? this.value : [ this.value ] ) : ( this.unknown ? [ this.unknown ] : [] )
